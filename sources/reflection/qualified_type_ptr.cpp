dec9d1e730c353a0eae97865b79ca04567f320de
Giuseppe Campana <giu.campana@gmail.com>
2015-10-07 08:39:31 +0200

    minor update (wip)

--- sources/reflection/qualified_type_ptr.cpp
+++ sources/reflection/qualified_type_ptr.cpp
@@ -5,7 +5,7 @@ namespace reflective
 	QualifiedTypePtr::QualifiedTypePtr(const Type * i_final_type, size_t i_indirection_levels, size_t i_constness_word, size_t i_volatileness_word)
 		: m_final_type(i_final_type), m_indirection_levels(i_indirection_levels), m_constness_word(i_constness_word), m_volatileness_word(i_volatileness_word)
 	{
-		REFLECTIVE_INTERNAL_ASSERT( i_indirection_levels <= s_max_indirection_levels, "The indirection levels exceed s_max_indirection_levels" );
+		REFLECTIVE_INTERNAL_ASSERT( i_indirection_levels <= s_max_indirection_levels, "The number of indirection levels exceeds s_max_indirection_levels" );
 		REFLECTIVE_INTERNAL_ASSERT( (i_constness_word & ~((2 << i_indirection_levels) - 1)) == 0, "The constness word is not valid" );
 		REFLECTIVE_INTERNAL_ASSERT( (i_volatileness_word & ~((2 << i_indirection_levels) - 1)) == 0, "The volatileness word is not valid" );
 	}
@@ -13,21 +13,31 @@ namespace reflective
 	QualifiedTypePtr::QualifiedTypePtr(const Type & i_final_type, ArrayView<CV_Flags> i_cv_flags)
 		: m_final_type(&i_final_type), m_indirection_levels(i_cv_flags.size()), m_constness_word(0), m_volatileness_word(0)
 	{
+		REFLECTIVE_ASSERT(i_cv_flags.size() <= s_max_indirection_levels, "The number of indirection levels exceeds s_max_indirection_levels");
+
+		uintptr_t constness_word, m_volatileness_word = 0;
+
 		for (CV_Flags flags : i_cv_flags)
 		{
 			if ((flags & CV_Flags::Const) != CV_Flags::None)
 			{
-				m_constness_word |= 1;
+				constness_word |= 1;
 			}
 
 			if ((flags & CV_Flags::Volatile) != CV_Flags::None)
 			{
-				m_volatileness_word |= 1;
+				olatileness_word |= 1;
 			}
 
-			m_constness_word <<= 1;
-			m_volatileness_word <<= 1;
+			constness_word <<= 1;
+			volatileness_word <<= 1;
 		}
+
+		m_constness_word = constness_word;
+		m_volatileness_word = volatileness_word;
+
+
+		REFLECTIVE_INTERNAL_ASSERT(m_constness_word == constness_word && m_volatileness_word == volatileness_word, "internal error");
 	}
 
 	bool QualifiedTypePtr::operator == (const QualifiedTypePtr & i_source) const
