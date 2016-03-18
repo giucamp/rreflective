cb0d5d033edac7246228e39a86f51c4cc4490faf
Giuseppe Campana <giu.campana@gmail.com>
2016-03-18 07:42:26 +0100

    bulk list update

--- sources/core/bulk_list.h
+++ sources/core/bulk_list.h
@@ -8,13 +8,13 @@ namespace reflective
 		return reinterpret_cast<TYPE*>((reinterpret_cast<uintptr_t>(i_address) + alignmentMask) & ~alignmentMask);
 	}
 
-	template <typename OTHER_TYPE, typename... CONSTR_PARAMS>
+	/*template <typename OTHER_TYPE, typename... CONSTR_PARAMS>
 		inline OTHER_TYPE * linear_alloc(void * & io_address, CONSTR_PARAMS && ...i_params)
 	{
 		OTHER_TYPE * result = new(address_upper_align<OTHER_TYPE>(io_address)) OTHER_TYPE(std::forward<CONSTR_PARAMS>(i_params)...);
 		io_address = result + 1;
 		return result;
-	}
+	}*/
 
 	template <typename TYPE>
 		class DefaultTypeInfo
@@ -38,6 +38,20 @@ namespace reflective
 			return DefaultTypeInfo(sizeof(COMPLETE_TYPE), std::alignment_of<COMPLETE_TYPE>::value, &MoverDestructorImpl < COMPLETE_TYPE >);
 		}
 
+		size_t size() const { return m_size; }
+		
+		size_t alignment() const { return m_alignment; }
+		
+		void destroy(TYPE * i_element)
+		{
+			(*m_mover_destructor)(nullptr, i_element);
+		}
+
+		void move_and_destroy(void * i_destination, TYPE * i_source_element)
+		{
+			(*m_mover_destructor)(i_destination, i_source_element);
+		}
+
 	private:
 
 		using MoverDestructorPtr = void (*)(void * i_dest_element, TYPE * i_source_element);
@@ -51,7 +65,7 @@ namespace reflective
 		{
 			if (i_destination != nullptr)
 			{
-				new (i_destination)COMPLETE_TYPE(std::move(*static_cast<COMPLETE_TYPE*>(i_source_element)));
+				new (i_destination) COMPLETE_TYPE(std::move(*static_cast<COMPLETE_TYPE*>(i_source_element)));
 			}
 			static_cast<COMPLETE_TYPE*>(i_source_element)->COMPLETE_TYPE::~COMPLETE_TYPE();
 		}
@@ -69,45 +83,53 @@ namespace reflective
 		template <typename... TYPES>
 			inline static BulkList make(TYPES... i_tuple)
 		{
-			const size_t buffer_size = RecursiveSize<0, TYPES...>::s_buffer_size;
-			const size_t buffer_alignment = RecursiveHelper<TYPES...>::s_item_alignment;
-			const size_t item_count = RecursiveHelper<TYPES...>::s_item_count;
+			size_t const buffer_size = RecursiveSize<0, TYPES...>::s_buffer_size;
+			size_t const buffer_alignment = RecursiveHelper<TYPES...>::s_item_alignment;
+			size_t const item_count = RecursiveHelper<TYPES...>::s_item_count;
 
-			void * dictionary = Alloc(item_count * sizeof(TYPE*) + item_count * sizeof(TYPE_INFO), 8);
-			TYPE * * item_dictionary = static_cast<TYPE * *>(dictionary);
-			TYPE_INFO * type_dictionary = reinterpret_cast<TYPE_INFO *>(item_dictionary + item_count);
-			
-			void * items = Alloc(buffer_size, buffer_alignment);
-			RecursiveHelper<TYPES...>::construct(item_dictionary, type_dictionary, items, std::forward<TYPES>(i_tuple)...);
+			BulkList new_list(InternalConstructorMemb);
 
-			return BulkList(InternalConstructor(), item_count, dictionary, items );
+			TYPE_INFO * const types = new_list.alloc_type_info_array(item_count);
+			void * const items = new_list.Alloc(buffer_size, buffer_alignment);
+			RecursiveHelper<TYPES...>::construct(types, items, std::forward<TYPES>(i_tuple)...);
+
+			new_list.m_size = item_count;
+			new_list.m_items = static_cast<TYPE*>( items );
+			new_list.m_types = types;
+
+			return std::move(new_list);
 		}
 
-		template <typename TYPE>
-			void push_back(const TYPE & i_source)
+		class iterator
 		{
+		public:
 
-		}
+			iterator(BulkList & i_source)
+				: m_curr_type(i_source.m_types), m_curr_item(i_source.m_items)
+			{
+
+			}
+
+			iterator & operator ++ ()
+			{
+				/*size_t const curr_size = m_curr_type->size();
+				m_curr_type = reinterpret_cast<uintptr_t>(m_curr_type) + curr_size;
+				return reinterpret_cast<TYPE*>((reinterpret_cast<uintptr_t>(i_address) + alignmentMask) & ~alignmentMask);*/
+				return *this;
+			}
+
+		private:
+			TYPE_INFO * m_curr_type;
+			TYPE * m_curr_item;
+		};
 
 	private:
 
-		TYPE * get_item_at(size_t i_index)
+		TYPE * get_items()
 		{
-			assert(i_index < m_size);
-			return static_cast<TYPE**>(m_dictionary)[i_index];
+			return m_items;
 		}
 
-		const TYPE * get_item_at(size_t i_index) const
-		{
-			assert(i_index < m_size);
-			return static_cast<TYPE**>(m_dictionary)[i_index];
-		}
-
-		const TYPE_INFO & get_type_info_at(size_t i_index)
-		{
-			assert(i_index < m_size);
-			TYPE_INFO * static_cast<TYPE**>(m_dictionary)[i_index];
-		}
 
 		static void * Alloc(size_t i_size, size_t i_alignment)
 		{
@@ -119,12 +141,32 @@ namespace reflective
 			return _aligned_free(i_block);
 		}
 
+		TYPE_INFO * alloc_type_info_array(size_t i_count)
+		{
+			using Allocator = typename std::allocator_traits<ALLOCATOR>::rebind_alloc<TYPE_INFO>;
+			Allocator alloc(*static_cast<const ALLOCATOR*>(this));
+			TYPE_INFO * new_array = alloc.allocate(i_count);
+			return new_array;
+		}
+
+		void * delete_type_info_array(TYPE_INFO * i_array)
+		{
+			using Allocator = typename std::allocator_traits<ALLOCATOR>::rebind_alloc<TYPE_INFO>;
+			Allocator alloc( *static_cast<const ALLOCATOR*>(this) );
+			alloc.deallocate(i_array);
+		}
+
 	private:
 
-		enum InternalConstructor { };
+		enum InternalConstructor { InternalConstructorMemb };
 
-		BulkList(InternalConstructor, size_t i_size, void * i_dictionary, void * i_items)
-			: m_size(i_size), m_dictionary(i_dictionary), m_items(i_items)
+		BulkList(InternalConstructor)
+		{
+
+		}
+
+		BulkList(InternalConstructor, const ALLOCATOR & i_allocator)
+			: ALLOCATOR(i_allocator)
 		{
 
 		}
@@ -150,11 +192,12 @@ namespace reflective
 			static const size_t s_item_alignment = std::alignment_of<FIRST_TYPE>::value > RecursiveHelper<OTHER_TYPES...>::s_item_alignment ?
 				std::alignment_of<FIRST_TYPE>::value : RecursiveHelper<OTHER_TYPES...>::s_item_alignment;
 
-			inline static void construct(TYPE * * & io_item_dictionary, TYPE_INFO * & io_type_dictionary, void * i_items, FIRST_TYPE && i_source, OTHER_TYPES && ... i_s1)
+			inline static void construct(TYPE_INFO * i_types, void * i_items, FIRST_TYPE && i_source, OTHER_TYPES && ... i_s1)
 			{
-				*io_item_dictionary++ = linear_alloc<FIRST_TYPE>(i_items, std::forward<FIRST_TYPE>(i_source));
-				new(io_type_dictionary++) TYPE_INFO( TYPE_INFO::make<FIRST_TYPE>() );
-				RecursiveHelper<OTHER_TYPES...>::construct(io_item_dictionary, io_type_dictionary, i_items, std::forward<OTHER_TYPES>(i_s1)...);
+				new(i_types) TYPE_INFO( TYPE_INFO::make<FIRST_TYPE>() );
+				FIRST_TYPE * const new_item = new(address_upper_align<FIRST_TYPE>(i_items)) FIRST_TYPE(std::forward<FIRST_TYPE>(i_source));
+				RecursiveHelper<OTHER_TYPES...>::construct(
+					i_types + 1, new_item + 1, std::forward<OTHER_TYPES>(i_s1)...);
 			}
 		};
 
@@ -164,16 +207,16 @@ namespace reflective
 			static const size_t s_item_count = 1;
 			static const size_t s_item_alignment = std::alignment_of<LAST_TYPE>::value;
 
-			inline static void construct(TYPE * * & io_item_dictionary, TYPE_INFO * & io_type_dictionary, void * i_items, LAST_TYPE && i_source)
+			inline static void construct(TYPE_INFO * i_types, void * i_items, LAST_TYPE && i_source)
 			{
-				*io_item_dictionary = linear_alloc<LAST_TYPE>(i_items, std::forward<LAST_TYPE>(i_source));
-				new(io_type_dictionary)TYPE_INFO(TYPE_INFO::make<LAST_TYPE>());
+				new(i_types) TYPE_INFO(TYPE_INFO::make<LAST_TYPE>());
+				new(address_upper_align<LAST_TYPE>(i_items)) LAST_TYPE(std::forward<LAST_TYPE>(i_source));
 			}
 		};
 
 	private:
 		size_t m_size;
-		void * m_dictionary;
-		void * m_items;
+		TYPE * m_items;
+		TYPE_INFO * m_types;
 	};
 }
